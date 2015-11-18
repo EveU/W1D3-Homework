083095c27a22830a94e0975e7f707a02ecdb8372
Evelyn Utterson <e.utterson@hotmail.co.uk>
2015-11-18 17:08:08 +0000

    Saved progress at end of class

--- bank_accounts_functions.rb
+++ bank_accounts_functions.rb
@@ -45,4 +45,59 @@ def number_of_bank_accounts()
   ACCOUNTS.length
 end
 
+def first_bank_account_holder()
+  ACCOUNTS.first[:holder_name]
+end
+
+def print_owner_names()
+  for acc in ACCOUNTS
+    puts acc[:holder_name]
+  end
+end
+
+def total_cash_in_bank()
+  total_amount = 0
+  for acc in ACCOUNTS
+    total_amount += acc[:amount]
+  end
+  return total_amount
+end
+
+def last_bank_account_holder()
+  ACCOUNTS.last[:holder_name]
+end
+
+def average_bank_account_value(total_amount, number_of_accounts)
+  (total_amount.to_f / number_of_accounts).round(2)
+end
+
+def total_cash_in_business_accounts()
+  business_total = 0
+  for acc in ACCOUNTS
+    business_total += acc[:amount] if acc[:type] == "business"
+  end
+  return business_total
+end
+
+def largest_bank_account_holder()
+  largest_account = {}
+  largest_account_amount = 0
+  for acc in ACCOUNTS
+    if acc[:amount] > largest_account_amount
+      largest_account = acc
+    end
+  end
+  return largest_account[:holder_name]
+end
+
+def largest_pers_acc_holder()
+  largest_account = {}
+  largest_account_amount = 0
+  for acc in ACCOUNTS
+      if (acc[:amount] > largest_account_amount) && (acc[:type] == "personal")
+        largest_account = acc
+      end
+    end
+    return largest_account[:holder_name]
+end
 
--- bank_accounts_runner.rb
+++ bank_accounts_runner.rb
@@ -5,30 +5,45 @@ bank_accounts_count = number_of_bank_accounts()
 puts "number of accounts #{bank_accounts_count}"
 puts "\n"
 
-# ##first account owner
-# first_bank_owner = first_bank_account_holder()
-# puts "first bank account owner is #{first_bank_owner}"
-# puts "\n"
+##first account owner
+first_bank_owner = first_bank_account_holder()
+puts "first bank account owner is #{first_bank_owner}"
+puts "\n"
 
-###print to screen name of all bank account owners
-# puts "name of bank acccount owners"
-# print_owner_names()
-# puts "\n"
+##print to screen name of all bank account owners
+puts "name of bank acccount owners"
+print_owner_names()
+puts "\n"
 
-###total cash in bank
-# total_cash = total_cash_in_bank()
-# puts "total in bank #{total_cash}"
-# puts "\n"
+##total cash in bank
+total_cash = total_cash_in_bank()
+puts "total in bank #{total_cash}"
+puts "\n"
 
-###last account owner
+##last account owner
+last_bank_owner = last_bank_account_holder()
+puts "last bank account owner is #{last_bank_owner}"
+puts "\n"
 
-###average bank account value
+##average bank account value
+average_account_value = average_bank_account_value(total_cash, bank_accounts_count)
+puts "average bank account value is #{average_account_value}"
+puts "\n"
 
-###total cash in business accounts
+##total cash in business accounts
+total_business_cash = total_cash_in_business_accounts()
+puts "total in business accounts #{total_business_cash}"
+puts "\n"
 
 ###holder of largest bank account
+largest_account_holder = largest_bank_account_holder()
+puts "#{largest_account_holder} has the largest bank account"
+puts "\n"
 
-###holder of largest personal account
+##holder of largest personal account
+largest_personal_acc_holder = largest_pers_acc_holder()
+puts "#{largest_personal_acc_holder} has the largest personal account"
+puts "\n"
 
 ###any other functionality you want to add.
 
